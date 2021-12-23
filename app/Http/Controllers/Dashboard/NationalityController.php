a2db21c04e6557f685be0a711ad4a9392cecd224
Mona Elshenawey <elshenaweymona92@gmail.com>
2021-12-23 12:12:32 +0200

    add bank to settings

--- app/Http/Controllers/Dashboard/NationalityController.php
+++ app/Http/Controllers/Dashboard/NationalityController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Dashboard\nationalityRequest;
-use App\Models\nationality;
+use App\Models\Nationality;
 use Illuminate\Http\Request;
 
 class NationalityController extends Controller
@@ -12,19 +12,19 @@ class NationalityController extends Controller
 
     public function index()
     {
-        $data['nationalities'] = nationality::latest()->paginate(200);
+        $data['nationalities'] = Nationality::latest()->paginate(200);
         return view('Dashboard.Nationalities.index', $data);
     }
 
     public function create()
     {
-        $data['latest_nationalities'] = nationality::orderBy('id', 'desc')->take(10)->get();
+        $data['latest_nationalities'] = Nationality::orderBy('id', 'desc')->take(10)->get();
         return view('Dashboard.Cities.create', $data);
     }
 
     public function store(nationalityRequest $request)
     {
-         $nationality= nationality::create($request->all());
+         $nationality= Nationality::create($request->all());
         return redirect()->route('nationalities.index')->with('message', trans('messages.messages.added_successfully'));
     }
 
@@ -33,21 +33,21 @@ class NationalityController extends Controller
         if (!nationality::find($id)) {
             return redirect()->route('dashboard.region.index')->with('class', 'danger')->with('message', trans('message.messages.try_2_access_not_found_content'));
         }
-        $data['latest_nationalities'] = nationality::orderBy('id', 'desc')->take(5)->get();
-        $data['nationality'] = nationality::find($id);
+        $data['latest_nationalities'] = Nationality::orderBy('id', 'desc')->take(5)->get();
+        $data['nationality'] = Nationality::find($id);
         return view('Dashboard.Cities.edit', $data);
     }
 
-    public function update(nationalityRequest $request, $id)
-    {
-        // nationality::findOrFail($id)->first()->fill($request->all())->save();
-        nationality::findOrFail($id)->update($request->all());
-        return redirect()->route('nationalities.index')->with('success', 'تم تعديل  بنجاح');
-    }
+//    public function update(nationalityRequest $request, $id)
+//    {
+//        // nationality::findOrFail($id)->first()->fill($request->all())->save();
+//        Nationality::findOrFail($id)->update($request->all());
+//        return redirect()->route('nationalities.index')->with('success', 'تم تعديل  بنجاح');
+//    }
 
     public function destroy(Request $request)
     {
-        $nationality = nationality::find($request->id);
+        $nationality = Nationality::find($request->id);
         if (!$nationality) return response()->json(['deleteStatus' => false, 'error' => 'Sorry, nationality is not exists !!']);
         try {
             $nationality->delete();
