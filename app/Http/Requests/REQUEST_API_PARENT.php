1807e274db249979b3c7a68ed6a53b1ecbecdc6b
Mona Elshenawey <elshenaweymona92@gmail.com>
2022-01-02 08:26:24 +0200

    send notifications

--- app/Http/Requests/REQUEST_API_PARENT.php
+++ app/Http/Requests/REQUEST_API_PARENT.php
@@ -21,6 +21,7 @@ class REQUEST_API_PARENT extends FormRequest
         $data['status'] = false;
         $data['message'] = $validator->errors()->first();
         $data['data'] = null;
-        throw new HttpResponseException(response()->json($data, JsonResponse::HTTP_UNPROCESSABLE_ENTITY)); // 422
+        throw new HttpResponseException(response()->json($data, JsonResponse::HTTP_OK)); // 200
+//        throw new HttpResponseException(response()->json($data, JsonResponse::HTTP_UNPROCESSABLE_ENTITY)); // 422
     }
 }
