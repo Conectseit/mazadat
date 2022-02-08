3d26380ec6bd937727f81325b420110a0bc584cf
Mona Elshenawey <elshenaweymona92@gmail.com>
2022-02-08 16:49:15 +0200

    splash

---
+++ database/migrations/2022_02_08_144146_add_name_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNameToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('full_name');
+            $table->string('first_name')->after('is_company')->nullable();
+            $table->string('middle_name')->after('first_name')->nullable();
+            $table->string('last_name')->after('middle_name')->nullable();
+            $table->string('address2')->after('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('full_name');
+            $table->dropColumn('first_name');
+            $table->dropColumn('middle_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('address2');
+        });
+    }
+}
