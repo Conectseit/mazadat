ed02d738cd7c3b5c5a13872a60ab01f9dfe90f78
Mona Elshenawey <elshenaweymona92@gmail.com>
2022-03-14 14:23:18 +0200

    d

--- resources/views/front/layouts/head.blade.php
+++ resources/views/front/layouts/head.blade.php
@@ -38,6 +38,21 @@
 
 
 
+{{--    // dropzon --}}
+
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+
+
+
+
+
+
 
 
     <title>Mazadat/@yield('title')</title>
