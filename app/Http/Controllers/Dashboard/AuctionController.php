c0944a373a2d28053e607d6974689f8ad1603e2a
Mona Elshenawey <elshenaweymona92@gmail.com>
2022-03-23 13:42:37 +0200

    updates

--- app/Http/Controllers/Dashboard/AuctionController.php
+++ app/Http/Controllers/Dashboard/AuctionController.php
@@ -279,7 +279,7 @@ class AuctionController extends Controller
     public function accept($id)
     {
         $auction = Auction::findOrFail($id);
-        if(is_null($auction->start_date) && is_null($auction->end_date ) )
+        if(($auction->start_date== null) && ($auction->end_date== null ) )
             {
                 return redirect()->route('auctions.index')->with('error', trans('messages.Sorry_you_should_complete_all_data_for_auction_first'));
             }
