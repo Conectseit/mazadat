1e0c6fe3666c14a6fe32ab18cf96691c3486655d
Mona Elshenawey <elshenaweymona92@gmail.com>
2022-03-27 14:19:29 +0200

    updates on appear auctions

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -18,14 +18,15 @@ class HomeController extends Controller
     {
         $on_progress_auctions = Auction::query()
 //            ->where('status','!=','done')->get();
-            ->where('status','=','on_progress')->where('end_date','<=' ,Carbon::now())->get();
+            ->where('status','=','on_progress')->get();
 
         foreach ($on_progress_auctions as $auction)
         {
-//            if(!$auction->end_date->isPast()) continue;
-//            if($auction->end_date <= Carbon::now()) continue;
+            if(!$auction->end_date->isPast()) continue;
+            if($auction->end_date <= Carbon::now()) continue;
             $auction->update(['status'=>'done']);
         }
+
     }
 
     public function cronJobAppearAuctions()
