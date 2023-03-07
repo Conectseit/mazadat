821cfbd95ea35625d58d5cf15832b8acf029c75d
Mona Elshenawey <elshenaweymona92@gmail.com>
2023-03-07 16:56:41 +0200

    commit

--- resources/views/front/general/blogs.blade.php
+++ resources/views/front/general/blogs.blade.php
@@ -5,6 +5,7 @@
         .our-blog .blog {
             position: relative;
             transition: all .5s ease-in-out;
+            width: 280px;
         }
 
         .blog .blog-img img {
@@ -14,7 +15,7 @@
         .blog .blog-img img {
             width: 100%;
             height: auto;
-            border-radius: 10px;
+            border-radius: 20px;
             -o-object-fit: cover;
             object-fit: cover;
         }
@@ -36,7 +37,10 @@
         .our-blog .blog .blog-des span {
             font-size: 15px;
             color: #8f98c5;
-            /*color: var(--paragraph);*/
+        }
+        .our-blog .blog:hover {
+            transform: scale(.85);
+            transition: .5s;
         }
     </style>
 
@@ -48,17 +52,15 @@
     </main>
     @include('front.layouts.parts.alert')
 
-    <section class="notifications-page" dir="{{ direction() }}">
+    <section class="notifications-page" dir="{{ direction() }}"  data-aos="fade-up">
         <div class="container">
             <div class="blog-page">
-
                 <br>
                 <br>
                 <br>
                 <div class="our-blog pad-40 fadeIn">
                     <div class="container">
                         <div class="row">
-                            {{--                            @foreach($blogs as $blog)--}}
                             @forelse($blogs as $blog)
 
                                 <div class="col-lg-4">
@@ -75,92 +77,9 @@
                                 <div style="text-align: center;">
                                     <h2> @lang('messages.there_is_no_blogs_yet') </h2></div>
                             @endforelse
-
-                                {{--                            @endforeach--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/ashl-7-khtoat-laaml-tsmym-anfografyk-ahtrafy-123"--}}
-                                {{--                                                          class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="أسهل 7 خطوات لعمل تصميم انفوجرافيك احترافي"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/ZGI0aA7M7HCUxGyvrHq4vlGQMBkZTQzlZKL5Rnpn.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>أسهل 7 خطوات لعمل تصميم انفوجرافيك احترافي</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2022-02-01</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/ttbyk-mnshat-121" class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="تطبيق منشآت"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/WVNaiiChA6elBGlN0oGaiACs3C7kSNK03yDngyge.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>تطبيق منشآت</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2022-01-09</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a--}}
-                                {{--                                        href="/blog/astdaf-moakaa-aloyb-aldlyl-alshaml-aan-astdaf-almoakaa-119" class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="استضافة مواقع الويب | الدليل الشامل عن استضافة المواقع"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/ggEiRTCHtEnfrhSVwtabytlo0wQYQDxyeg9d8Xyt.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>استضافة مواقع الويب | الدليل الشامل عن استضافة المواقع</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2022-01-04</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/afdl-3-fydyohat-tkhlyk-tfhm-tsoyk-sh-117" class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="أفضل 3 فيديوهات تخليك تفهم تسويق صح"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/DykwXf5rvtSU4Sow8vCG3SYK9AOXX8C8wm7rduWG.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>أفضل 3 فيديوهات تخليك تفهم تسويق صح</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2021-12-30</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/afdl-khdm-tsmym-mtagr-alktrony-mn-alaaalmy-alhr-115"--}}
-                                {{--                                                          class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="أفضل خدمة تصميم متاجر الكترونية من العالمية الحرة"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/pTCnRVjBV8ScHamcB8eb6MQesat8uocAqIZ0nkcU.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>أفضل خدمة تصميم متاجر الكترونية من العالمية الحرة</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2021-12-19</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/alaaalmy-alhr-afdl-shrk-tsmym-ttbykat-algoal-113"--}}
-                                {{--                                                          class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="العالمية الحرة أفضل شركة تصميم تطبيقات الجوال"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/BKkcdEch31R0A74K78T3wwrOiEJBzGCXVXw0Hy7y.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>العالمية الحرة أفضل شركة تصميم تطبيقات الجوال</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2022-01-18</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/ttbyk-amn-111" class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="تطبيق آمنة"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/A99ZDq1D8mke6BK2n1jvwo7o6XyQxTcRM9aJchth.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>تطبيق آمنة</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2021-12-14</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-                                {{--                            <div class="col-lg-4">--}}
-                                {{--                                <div class="blog mb-4"><a href="/blog/braingate-altknologya-altby-althory-109" class="">--}}
-                                {{--                                        <div class="blog-img"><img alt="BrainGate التكنولوجيا الطبية الثورية"--}}
-                                {{--                                                                   src="https://alalmyia.t-demo.alalmiyalhura.com/storage/images/blog/peEKEDCXT8o8EiDt3I3gL3XfHXDq4wpHkTDhPG6d.jpg"--}}
-                                {{--                                                                   class="lazyLoad isLoaded"></div>--}}
-                                {{--                                        <div class="blog-des"><h4>BrainGate التكنولوجيا الطبية الثورية</h4>--}}
-                                {{--                                            <p class="first-p"></p> <span>2021-11-24</span></div>--}}
-                                {{--                                    </a></div>--}}
-                                {{--                            </div>--}}
-
-
                         </div>
                     </div>
                 </div>
-                {{--                <div class="cookieControl__BlockedIframe" height="0" width="0" style="display: none; visibility: hidden;"><p>--}}
-                {{--                        لرؤية هذا ، يرجى تمكين ملفات تعريف الارتباط الوظيفية--}}
-                {{--                        <a href="#">هنا</a></p></div>--}}
             </div>
         </div>
     </section>
