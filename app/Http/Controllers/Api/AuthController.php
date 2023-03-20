b7de025b1c89a7ddc424dfd5e870cd80780a1841
Mona Elshenawey <elshenaweymona92@gmail.com>
2023-03-20 17:16:41 +0200

    commit

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -165,7 +165,9 @@ class AuthController extends PARENT_API
         $code = create_rand_numbers();
 
         $user->update(['reset_password_code' => $code]);
-        SmsController::send_sms(($request->mobile), trans('messages.activation_code_is', ['code' => $code]));
+        SmsController::sendSms(($request->mobile), trans('messages.activation_code_is', ['code' => $code]));
+
+//        SmsController::send_sms(($request->mobile), trans('messages.activation_code_is', ['code' => $code]));
         return responseJson(true, trans('api.send_token'), $code); //
     }
 
