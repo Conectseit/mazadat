1a28cc0ef04410f4254b465481773baf214b27f3
Mona Elshenawey <elshenaweymona92@gmail.com>
2023-05-04 18:03:45 +0200

    commit

--- resources/views/front/auctions/auction_details.blade.php
+++ resources/views/front/auctions/auction_details.blade.php
@@ -432,6 +432,65 @@
 
         </section>
 
+
+        <!-- upload-file-modal -->
+        <div class="modal user-modal bio-modal fade" id="upload-file-modal" tabindex="-1"
+             aria-labelledby="exampleModalLabel"
+             aria-hidden="true" dir="{{ direction() }}">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content" >
+                    <form action="{{route('front.addFile')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <input type="hidden" name="auction_id" value="{{$auction->id}}">
+
+                            <div class="form-group ">
+                                <div class="col-lg-12 col-md-12">
+                                    <select name="file_name_id" class="form-select  form-control">
+                                        <option selected
+                                                disabled>{{trans('messages.select_file_name')}}</option>
+                                        @foreach ($inspection_file_names as $inspection_file_name)
+                                            <option
+                                                value="{{ $inspection_file_name->id }}"> {{ $inspection_file_name->name }} </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <br>
+
+                            <div class="form-group">
+                                <div class="col-lg-12">
+                                    <input type="file"  class="form-control" name="inspection_report_images">
+                                </div>
+                                @error('inspection_report_images')<span
+                                    style="color: #e81414;">{{ $message }}</span>@enderror
+                            </div>
+                            <br>
+
+                            <div class="form-group ">
+                                <div class="col-lg-12 col-md-12">
+                            <textarea  cols="60" name="description" placeholder="{{trans('messages.enter_message')}} "required>
+                            </textarea>
+                                    @error('description')<span
+                                        style="color: #e81414;">{{ $message }}</span>@enderror
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary add">{{trans('messages.send')}}</button>
+                            <button type="button" class="btn btn-secondary cancel" data-bs-dismiss="modal">{{trans('messages.cancel')}}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- upload-file-modal -->
     </div>
 
 @stop
