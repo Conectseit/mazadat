81e347708a4e12ceb487df543e0068ddb01fe702
Mona Elshenawey <elshenaweymona92@gmail.com>
2023-05-15 14:43:47 +0200

    Initial commit

--- app/Http/Controllers/Dashboard/NotificationController.php
+++ app/Http/Controllers/Dashboard/NotificationController.php
@@ -40,7 +40,7 @@ class NotificationController extends Controller
                     'title' => $message->title,
                     'text' => $request->text,
                 ]);
-                SmsController::sendSms($user->mobile, $request->text);
+//                SmsController::sendSms($user->mobile, $request->text);
             }
             return back()->with('class', 'success')->with('message', trans('messages.messages.send_successfully'));
         }
@@ -106,7 +106,7 @@ class NotificationController extends Controller
             'text' => $request->text,
         ]);
 
-        SmsController::sendSms($user->mobile, $request->text);
+//        SmsController::sendSms($user->mobile, $request->text);
         return back()->with('class', 'success')->with('message', trans('messages.messages.send_successfully'));
     }
 }
