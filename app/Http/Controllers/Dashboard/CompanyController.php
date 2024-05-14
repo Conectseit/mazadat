8697e048f0296b3cfa40b59ee1d3b1b68aa9101d
AbdoallhReda <50003039+AbdoallhReda@users.noreply.github.com>
2024-05-14 17:33:40 +0300

    company

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -189,7 +189,7 @@ class CompanyController extends Controller
     {
         $company = User::findOrFail($id);
         $company->update(['is_accepted'=> 1,'is_verified'=> 1]);
-        Notification::sendAcceptAccountNotify($company->id);
+        (new \App\Models\Notification)->sendAcceptAccountNotify($company->id);
 
         SmsController::sendSms($company->mobile, 'تم قبول حسابك  من ادرة موقع مزادات' );
 
@@ -199,7 +199,7 @@ class CompanyController extends Controller
     {
         $company = User::findOrFail($id);
         $company->update(['is_accepted'=> 0]);
-        Notification::sendNotAcceptAccountNotify($company->id);
+        (new \App\Models\Notification)->sendNotAcceptAccountNotify($company->id);
 
         SmsController::sendSms($company->mobile, 'هناك خطأ في تكملة بيانات حسابك في موقع مزادات من فضلك ارسلها مرة اخري' );
         return back()->with('success',  trans('messages.not_verified_yet_and_send_SMS'));
